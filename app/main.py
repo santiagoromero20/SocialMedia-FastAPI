84d62381658d667bd148b68ab9ef4a6261d577d6
Santi Romero <santiagooromero1234@gmail.com>
2022-10-31 18:28:52 -0300

    App folder

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from . import models
+from .database import engine
+from .routers import post, user, auth, vote
+
+models.Base.metadata.create_all(bind=engine)
+
+#App Object Declaration
+app = FastAPI()
+
+origins=["*"]
+#Add Middle
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins, #Requests Allowed
+    allow_credentials=True,
+    allow_methods=["*"], #Methods Allowed
+    allow_headers=["*"]  #Headers Allowed
+)
+
+#Importing Routers
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
+
+
+
---
+++ app/routers/vote.py
@@ -0,0 +1,48 @@
+from fastapi import status, HTTPException, Depends, APIRouter
+from sqlalchemy.orm import Session
+from .. import schemas, database, models, oauth2
+
+
+router = APIRouter(
+    prefix="/vote",
+    tags=['Vote']
+)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+
+    #Querying Post
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {vote.post_id} does not exist")
+
+    #Querying Vote
+    vote_query = db.query(models.Vote).filter(
+        models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
+
+    #Search if that user has already liked this post or not
+    found_vote = vote_query.first()
+    if (vote.dir == 1):
+        #If the post is liked, raise HTTPException message
+        if found_vote:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                                detail=f"user {current_user.id} has alredy voted on post {vote.post_id}")
+
+        #If it isn´t like, create a new vote/like
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "successfully added vote"}
+
+    #Logic to delete a pre-existing vote
+    else:
+        if not found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist")
+
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+
+        return {"message": "successfully deleted vote"}
